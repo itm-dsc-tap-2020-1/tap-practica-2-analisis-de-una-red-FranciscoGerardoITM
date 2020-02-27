0ce639c47d11a20b17d8de16f76c461b50382b1e
FranciscoGerardoITM <61289081+FranciscoGerardoITM@users.noreply.github.com>
2020-02-27 11:18:41 -0600

    Add files via upload

---
+++ Practica2.py
@@ -0,0 +1,9 @@
+import os
+
+hostname = "www.itmorelia.edu.mx"
+respuesta = os.system("ping -c 1 "+hostname)
+
+if respuesta==0:
+    print(hostname + ": esta en funcionamiento!")
+else:
+    print(hostname + ": no funciona!")
